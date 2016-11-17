e8f8c2b661da55aee028623d135329ff4c9c2f58
Oleg Tofan <olezzz@gmail.com>
2016-11-17 22:26:55 +0200

    Now CaculatorProxy automatically creates Real Subject to be addressed

--- src/calc/proxy/CalculatorProxy.java
+++ src/calc/proxy/CalculatorProxy.java
@@ -6,27 +6,27 @@ package calc.proxy;
 public class CalculatorProxy implements CalculatorInterface {
     private Calculator calc;
 
+    public CalculatorProxy() {
+        calc = new Calculator();
+    }
+
     @Override
     public double add(double x, double y) {
-        if (calc == null) {calc = new Calculator();}
         return calc.add(x, y);
     }
 
     @Override
     public double sub(double x, double y) {
-        if (calc == null) {calc = new Calculator();}
         return calc.sub(x, y);
     }
 
     @Override
     public double mult(double x, double y) {
-        if (calc == null) {calc = new Calculator();}
         return calc.mult(x, y);
     }
 
     @Override
     public double div(double x, double y) {
-        if (calc == null) {calc = new Calculator();}
         return calc.div(x, y);
     }
 }
